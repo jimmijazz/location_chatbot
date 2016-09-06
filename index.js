d51d052a70ce00cf181c728cb75fe4810353ebfe
jimmijazz <joshua.bitossi@gmail.com>
2016-09-07 00:28:22 +1000

    index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/webhook', function( req, res){
 
 // Handler recieving messages
 app.post('/webhook', function (req, res) {
-  var events = req.body.entry[0].messaging; // messaging is a property of the req that batches the entire text sent
+  var events = req.body.entry[0]; // messaging is a property of the req that batches the entire text sent
   for (i = 0; i < events.length; i++) {
     var event = events[i];
     console.log(event);
