cad5c03e1287f1818bc84fc449c100414b5802ea
jimmijazz <joshua.bitossi@gmail.com>
2016-09-07 10:23:52 +1000

    index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.post('/webhook', function (req, res) {
         var event = events[i];
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+            console.log(event.message)
         }
     }
     res.sendStatus(200);
