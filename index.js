f6bc9a2593df0d56f42b1f2b6077c7c30507e3c7
jimmijazz <joshua.bitossi@gmail.com>
2016-09-11 00:56:02 +1000

    mongoDB

--- index.js
+++ index.js
@@ -54,10 +54,11 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        var id = event.sender.id;
-        var message = event.message.text;
+
 
         if (event.message && event.message.text) {
+            var id = event.sender.id;
+            var message = event.message.text;
             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
 
           } else if (event.message && event.message.attachments) {
