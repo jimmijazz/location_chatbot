a8f2dcbbc01be40cac209eca3f91237cbf466ceb
jimmijazz <joshua.bitossi@gmail.com>
2016-09-13 20:20:14 +1000

    added user profile get

--- index.js
+++ index.js
@@ -148,7 +148,6 @@ function sendGeneric(recipientId, location, image_url){
 function userProfile(userId){
   // Returns dict of user profile
   // userProfile(str) -> dict(first_name:str,last_name:str,profile_pic:str,locale:str,timezone:int,gender:str)
-  var user = {}
   request({
     url: 'https://graph.facebook.com/v2.6/'+userId+'?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=PAGE_ACCESS_TOKEN"',
     qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
@@ -161,10 +160,9 @@ function userProfile(userId){
       console.log('Error: ', response.body.error);
     }
 
-    user = JSON.parse(response.body);
+    var user = JSON.parse(response.body);
     console.log(typeof user);
     return user;
   })
-  return user;
 
 }
