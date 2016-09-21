7685626290e607696c6f8506fc10b870d980860f
jimmijazz <joshua.bitossi@gmail.com>
2016-09-21 16:57:46 +1000

    updated

--- index.js
+++ index.js
@@ -170,26 +170,6 @@ function sendMessage(recipientId, message) {
 
 
 function createLocation(recipientId, message) {
-  // Send message requesting address
-  var user_id = ""
-
-  request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: {
-          recipient: {id: recipientId},
-          message: message,
-      }
-  }, function(error, response, body) {
-      if (error) {
-          console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-      }
-      user_id = body.
-  });
-  sendMessage(recipientId, message);
 
   // Check if it is a location
 
@@ -198,7 +178,8 @@ function createLocation(recipientId, message) {
   // Else return error message "not an address"
 
 
-}
+};
+
 function sendGeneric(recipientId, location, image_url){
   //https://developers.facebook.com/docs/messenger-platform/send-api-reference/generic-template
   request({
