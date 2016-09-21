0e5c62b139ba9fbb58a198e424ee0dfb6ba01281
jimmijazz <joshua.bitossi@gmail.com>
2016-09-21 16:56:37 +1000

    updated

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     var collection = db.collection(CONTACTS_COLLECTION);
     var houses_collection = db.collection(HOUSES_COLLECTION);
-
+    console.log(events);
 
 
     for (i = 0; i < events.length; i++) {
@@ -171,6 +171,24 @@ function sendMessage(recipientId, message) {
 
 function createLocation(recipientId, message) {
   // Send message requesting address
+  var user_id = ""
+
+  request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+      method: 'POST',
+      json: {
+          recipient: {id: recipientId},
+          message: message,
+      }
+  }, function(error, response, body) {
+      if (error) {
+          console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+      }
+      user_id = body.
+  });
   sendMessage(recipientId, message);
 
   // Check if it is a location
