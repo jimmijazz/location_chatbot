a5f93e070cc2edcaa0feda16c851ce5cef47c191
jimmijazz <joshua.bitossi@gmail.com>
2016-09-21 17:42:55 +1000

    updated

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ function sendMessage(recipientId, message) {
 function isAgent(id) {
 
   // Check the user is allowed to create a location. Replace with DB lookup
-  if (id === "1285458091486614") {
+  if (db.collection(AGENTS).find({"_id" : id}) {
     sendMessage(id, {text: "You are allowed to create inspections, please send your address"});
 
     // Update agent status to creating inspection
