ae65272c30b621eaa7073b443230a450d240150c
jimmijazz <joshua.bitossi@gmail.com>
2016-10-01 19:48:53 +1000

    added createInspection

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const findOrCreateSession = (fbid) => {
 
 // Bot actions
 const actions = {
-  send({sessionId}, text} {
+  send({sessionId}, text) {
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
     const recipientId = sessions[sessionId].fbid;
