386f79e2664911530d565ac78339e74c5b24833a
jimmijazz <joshua.bitossi@gmail.com>
2016-10-01 19:36:54 +1000

    added createInspection

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ const actions = {
                         	}
 
                         };
-                        context.property = message;
+                        context.property = "Checking in";
 
                   })
 
