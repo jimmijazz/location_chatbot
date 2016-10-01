57eff524a8569e6456e7be478779997b607c9654
jimmijazz <joshua.bitossi@gmail.com>
2016-10-01 11:19:41 +1000

    added createInspection

--- index.js
+++ index.js
@@ -213,8 +213,8 @@ const actions = {
   // Sends a generic template message for the user to check into that property
   checkIn({context, entities}) {
     return new Promise(function(resolve, reject) {
-      console.log("entities: " + entities);
-      console.log("context: " + context);
+      console.log("entities: " + JSON.stringify(entities, null, 4));
+      console.log("context: " + JSON.stringify(context, null, 4));
       var address = firstEntityValue(entities, "location");
 
       if (address) {
