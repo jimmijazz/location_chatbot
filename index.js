324d4b5c985b00b4f34cb7c7de24a756261b63b7
jimmijazz <joshua.bitossi@gmail.com>
2016-10-03 11:37:18 +1000

    payload

--- index.js
+++ index.js
@@ -241,9 +241,9 @@ const actions = {
                           console.log("Found property in PROPERTIES collection")
                           // For some reason it sends payload as a str so have to try and convert to JSON or split
                           var payload =
-                          ("{" + "title" + ":" + address.formatted_address,
-                            "subtitle" + ":" + prop_result.description,
-                            "image_url" + ":" + prop_result.photos[0],
+                          ("{" + "title" + ":" + address.formatted_address+","+
+                            "subtitle" + ":" + prop_result.description+","+
+                            "image_url" + ":" + prop_result.photos[0]+","+
                             "buttons" + ":" + "[{" +
                               "type" + ":" + "postback," +
                                "title" + ":" + "Check In" +
