334f2d287c3932ca34dd5c009cfedbfa396fa756
jimmijazz <joshua.bitossi@gmail.com>
2016-10-05 14:55:55 +1000

    Find property by location

--- index.js
+++ index.js
@@ -491,7 +491,7 @@ app.post('/webhook', function (req, res) {
                     } else if (result) {
                       console.log("Property found by location!" + result);
                       var location_image = map_url + lat + "," + long + "&zoom=" + 20 + "&size=640x400&key=" + google_api_key;
-                      var payload = string([{
+                      var payload = String([{
                         "title" : address.formatted_address,
                         "subtitle" : address.formatted_address,
                         "image_url" : result.photos[0],
