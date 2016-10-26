f117bc257d9fe3f0cba5107a4855dabab202b221
jimmijazz <joshua.bitossi@gmail.com>
2016-10-26 22:05:31 +1000

    Moved update message

--- index.js
+++ index.js
@@ -744,7 +744,7 @@ function updateMsg(id, msg) {
     if(count === 0) {
 
       // 1a. If new user, insert into database
-      db.collection(PEOPLE).insert({_id : id, messages:msg_meta, function(err, result) {
+      db.collection(PEOPLE).insert({_id : id, messages:msg, function(err, result) {
         if (err) {
           console.log("Error updating PEOPLE. Error: ", err)
         } else {
@@ -754,7 +754,7 @@ function updateMsg(id, msg) {
       });
       // 1.b If not new user, update user messages
     } else {
-        db.collection(PEOPLE).update({_id: id}, { $push: {messages: msg_meta}}, function(err, result){
+        db.collection(PEOPLE).update({_id: id}, { $push: {messages: msg}}, function(err, result){
           if (err) {
             console.log("Error updating msg_meta. Error: ", err);
           } else {
