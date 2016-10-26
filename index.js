9a062952c1242825cc7b4d061c6ecae003c43027
jimmijazz <joshua.bitossi@gmail.com>
2016-10-26 18:43:24 +1000

    Get WIT working

--- index.js
+++ index.js
@@ -234,9 +234,12 @@ const actions = {
   },
 
   // Sends a generic template message for the user to check into that property
-  checkIn({sessionId}, {context, entities}) {
+  checkIn({context, entities}) {
     return new Promise(function(resolve, reject) {
       const recipientId = sessions[sessionId].fbid;
+      for(var prop in context){
+        console.log(prop);
+      };
 
       // Text version of address ie: 12 Mascot Street, Upper Mount Gravatt
       var address = firstEntityValue(entities, "location");
