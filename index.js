4147e94703823e5d577d927d6e6a6f10772fb644
jimmijazz <joshua.bitossi@gmail.com>
2016-10-27 23:14:23 +1000

    gallery

--- index.js
+++ index.js
@@ -800,7 +800,7 @@ function isAgent(id) {
 //                   '"image_url"' + ":" + '"' + "http://cdn1.ljhooker.com/57874eaf7bd719e719000279.jpg" + '"' +
 //                   "}"
 //                 ;
-const get_started = JSON.stringify([{
+const get_started = ([{
   "title" : "146/54 Slobodian Avenue",
   "subtitle" : " Striking residential building",
   "image_url" :  "http://cdn1.ljhooker.com/57874eaf7bd719e719000279.jpg"
